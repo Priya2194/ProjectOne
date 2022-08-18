c067c8d64c670b757454f7acaab89a6e1ab340e6
Priyanka <ni3priyab3patil@gmail.com>
2022-08-19 00:05:53 +0530

    ProjectOne

---
+++ app/src/main/java/com/example/projectone/CustomToastActivity.java
@@ -0,0 +1,28 @@
+package com.example.projectone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class CustomToastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_toast);
+
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.custom_layout, (ViewGroup) findViewById(R.id.layout_id));
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setView(view);
+        toast.show();
+    }
+}
